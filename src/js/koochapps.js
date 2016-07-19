75267187eba11ebc9826c61f00a7e1ba412a2cde
Gaston Kaltner <gaston.kaltner@globallogic.com>
2016-07-19 16:24:56 -0300

    remove conections to gmail

--- src/js/koochapps.js
+++ src/js/koochapps.js
@@ -2,8 +2,6 @@
  * Created by gaston on 20/06/16.
  */
 
-var apiKey = "AIzaSyC09rRuL8LrQ4NoE6b6PYHER8mKg5ATVDM";
-
 $(document).ready(function(){
     $('.parallax').parallax();
 
@@ -50,57 +48,6 @@ $(document).ready(function(){
         }
     }, 3000);
 });
-
-
-/*var options = [
-    {selector: '.cartas', offset: 0, callback: function() {
-        Materialize.showStaggeredList('.cartas');
-    }}
-];
-Materialize.scrollFire(options);*/
-
-function handleClientLoad() {
-    gapi.client.setApiKey(obj.api);
-    window.setTimeout(checkAuth, 1);
-}
-function checkAuth() {
-    gapi.auth.authorize({
-        client_id: obj.client_id,
-        scope: obj.scopes,
-        immediate: true
-    }, handleAuthResult);
-}
-function handleAuthResult(authResult){
-    console.log(authResult);
-}
-
-function sendEmail(){
-    sendMessage({
-        'Subject':'esto es un asunto',
-        'From':'gaston.kaltner@gmail.com'
-    },"esto es un mensaje",alert("se envio con exito"));
-}
-
-function sendMessage(headers_obj, message, callback) {
-    var email = '';
-    for(var header in headers_obj)
-        email += header += ": "+headers_obj[header]+"\r\n";
-    email += "\r\n" + message;
-    var sendRequest = gapi.client.gmail.users.messages.send({
-        'userId': 'me',
-        'resource': {
-            'raw': window.btoa(email).replace(/\+/g, '-').replace(/\//g, '_')
-        }
-    });
-    return sendRequest.execute(callback);
-}
-
-var obj = {
-    api:"AIzaSyCFqxVIuHqZd_Odmp-sgUbJpUZEQvgNGV0",
-    client_id:"756788320951-p8jjvfand90vc2f6ampptq1mpqle1fur.apps.googleusercontent.com",
-    scopes:'https://www.googleapis.com/auth/gmail.send'
-};
-
 
 /**
  * Scroll smooth
